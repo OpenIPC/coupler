cb4771fd567fc6424cd74b7980d94ed1bae8be29
Dmitry Ermakov <de@dimer.org.ua>
2021-12-14 18:13:54 +0300

    Skip extracting md5sums from archive

--- .github/workflows/_xm2oipc.sh
+++ .github/workflows/_xm2oipc.sh
@@ -37,7 +37,7 @@ IFS=" "
 mkdir -p ${WORKDIR}
 mkdir -p ${OUTPUTDIR}
 
-tar -xvz -f openipc.${SOC}-br.tgz -C ${WORKDIR}/
+tar -xvz -f openipc.${SOC}-br.tgz -C ${WORKDIR}/ --exclude "*.md5sum"
 
 # Check if give files exceed partition boundaries
 [[ $(stat --printf="%s" ${WORKDIR}/rootfs*) -gt $(($ROOTFS_E - $ROOTFS_A)) ]] || [[ $(stat --printf="%s" ${WORKDIR}/uImage*) -gt $(($KERNEL_E - $KERNEL_A)) ]] && echo "Filesize exceeds boundaries" && exit 1
